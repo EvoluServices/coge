d58a670c4928d07821b0c8db3ccc0f2288e75b62
Felipe Milani <felmilani@gmail.com>
2022-04-14 13:29:36 -0300

    Add spinner feedback on each step of generate command

---
+++ packages/design-system/index.tsx
@@ -0,0 +1,7 @@
+
+export * from "./Teste";
+export * from "./Teste";
+export * from "./Teste";
+export * from "./Teste";
+export * from "./Teste";
+export * from "./Teste";
--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -8,25 +8,32 @@ module.exports = {
       parameters,
       template: { generate },
       patching: { append },
-      print: { info },
+      print: { highlight, spin, success },
     } = toolbox
 
     const name = parameters.first
 
+    highlight(`Gerando arquivos do componente ${name}:\n`)
+    let spinner = spin('Gerando o componente')
     await generate({
       template: 'component.tsx.ejs',
       target: `packages/design-system/${name}.tsx`,
       props: { name },
     })
+    spinner.succeed()
 
+    spinner = spin('Gerando as histórias')
     await generate({
       template: 'stories.tsx.ejs',
       target: `packages/design-system/stories/${name}.stories.tsx`,
       props: { name },
     })
+    spinner.succeed()
 
+    spinner = spin('Exportando o componente')
     await append('packages/design-system/index.tsx', `export * from "./${name}";\n`)
+    spinner.succeed()
 
-    info(`Generated component ${name}`)
+    success('\nUhul! Tudo certo. Partiu customizar ele 🚀')
   },
 }
